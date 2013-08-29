9a7dfdb57b5acaee2f5e778565d50dbfa641d454
mdholland <mdholland@ucdavis.edu>
2013-08-29 16:06:00 -0700

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,6 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  # Add the Capybara domain specific language (DSL).
+  config.include Capybara::DSL
 end
